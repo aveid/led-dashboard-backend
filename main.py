a8c16198ef65e6e152ef44db5b5ea0fe5c8e2cbc
ntabyldieva <ntabyldieva@megacom.kg>
2026-07-17 11:15:57 +0600

    ref: cors

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def create_app() -> FastAPI:
     # CORS: разрешаем запросы фронтенда (Flutter Web) с указанных адресов.
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=settings.cors_origins or ["*"],
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
